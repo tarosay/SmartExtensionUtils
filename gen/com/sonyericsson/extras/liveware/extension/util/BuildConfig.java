c9dd6f0a17ee145b986ab02fdc9b1d15e3911829
Tarosa <tarosa.y@gmail.com>
2012-04-16 04:37:42 +0900

    Second commit

---
+++ gen/com/sonyericsson/extras/liveware/extension/util/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.sonyericsson.extras.liveware.extension.util;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
